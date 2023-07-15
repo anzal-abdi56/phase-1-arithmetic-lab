356758ffa51fd06ab77cc99cbfa4222eb4520b59
+ <+>
2023-07-15 05:29:31 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+let num3 = 14
+let num4 = 10
+let mod = num3 % num4
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) 
+let max = Math.max(4, 6, 10, 16, 20)
 
-// Write your code here
